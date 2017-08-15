a1b0fbeb3b79a534d98cfadd0a761b22898569b2
12FrosT <ashishchauhan1206@gmail.com>
2017-08-15 22:52:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,52 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone={
+  title:'Article-one',
+  heading:'Article one',
+  content:`<p>
+                this is the content of frist article.
+            </p>
+            <p>
+                this other paragraph2
+            </p>
+            <p>
+                this is other paragraph3
+            </p>`
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+var htmkTemplate=`
+    <html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewpoint" content="with device-width, inital scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class ="container">
+        <div>
+            <a href ="/">Home</a>
+        </div>
+        <hr>
+        <h3>${heading} </h3>
+        <div>
+        ${content}
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/article-one',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleone));
 });
 
 app.get('/article-two',function(req, res){
