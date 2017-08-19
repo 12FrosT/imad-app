43f42a71e232aa4a147b1be1c2d96bc57b04b9e7
12FrosT <ashishchauhan1206@gmail.com>
2017-08-19 12:27:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,6 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML="new text";
+
+var element = document.getElementById('imag');
+element.style.marginLeft='100px';
