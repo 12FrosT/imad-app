4d8064de95401211a043557ebe036d4f761dff7b
12FrosT <ashishchauhan1206@gmail.com>
2017-08-19 15:43:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,15 @@ var element = document.getElementById('main-text');
 element.innerHTML="new text";
 
 var img = document.getElementById('imag');
-var marginLeft=0;
+var marginLeft=0,a=1;
 function moveRight(){
+    if(marginLeft===0)
+    a=1;
+    if(marginLeft==50)
+    a=0;
+    if(a===0)
+    marginLeft=marginLeft-1;
+    else
     marginLeft=marginLeft + 1;
     img.style.marginLeft=marginLeft +'px';
 }
