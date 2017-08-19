653a9a9e3a15d46a058455e74ec1dafbf6ad2c30
12FrosT <ashishchauhan1206@gmail.com>
2017-08-19 13:44:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ var element = document.getElementById('main-text');
 element.innerHTML="new text";
 
 var img = document.getElementById('imag');
-img.onclick = fuction (){
+img.onclick = function (){
     img.style.marginLeft='100px';
-}
+};
