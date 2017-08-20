5d88e12381912fe49cfe0c85b09ebbc85cfa238a
12FrosT <ashishchauhan1206@gmail.com>
2017-08-20 16:22:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,6 +24,7 @@ button.onclick=function(){
         if(request.readystate===XMLHttpRequest.done){
             
             if(request.status===200){
+                var counter=request.responseText;
                 var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
             }
