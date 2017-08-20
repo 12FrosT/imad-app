2827c0efce629bc4a6137533f4c1c7b951333e79
12FrosT <ashishchauhan1206@gmail.com>
2017-08-20 15:56:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,5 @@ button.onclick=function(){
     
     counter = counter+1;
     var span=document.getElementById('count');
-    span.onclick=function(){
-        counter=counter+1;
         span.innerHTML=counter.toString();
-    };
 };
