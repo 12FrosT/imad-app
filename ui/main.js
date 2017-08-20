e4796159501984dffdd5bc2f0578e54c113259cf
12FrosT <ashishchauhan1206@gmail.com>
2017-08-20 16:18:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,14 +17,19 @@ img.onclick = function (){
     var interval=setInterval(moveRight,50);
 };
 
-var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(reques.readystate===XMLHttpRequest.done){
+            
+            if(request.status===200){
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    reuqest.open(GET,'http://ashishchauhan1206.imad.hasura-app.io/counter',true);
+    request.send(null);
     
-    
-    
-    
-    counter = counter+1;
-    var span=document.getElementById('count');
-        span.innerHTML=counter.toString();
 };
