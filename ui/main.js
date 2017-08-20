e0c18cad937fbfb83b0f048768b2868eab7e2d40
12FrosT <ashishchauhan1206@gmail.com>
2017-08-20 15:50:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,5 @@
 console.log('Loaded!');
 
-
-var element = document.getElementById('main-text');
-element.innerHTML="new text";
-
 var img = document.getElementById('imag');
 var marginLeft=0,a=1;
 function moveRight(){
