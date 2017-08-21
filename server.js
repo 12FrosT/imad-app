e605d841e3913b5dcdba5e501bd77937e4b4d476
12FrosT <ashishchauhan1206@gmail.com>
2017-08-21 14:26:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    var name=req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(name));
+});
 var articles = {
     'article-one' : {
       title:`Article-one`,
