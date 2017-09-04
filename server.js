b83f349c868cfd0369ff6a0a6c3fc8698d976cfd
ASHISH <ashishchauhan1206@gmail.com>
2017-09-04 20:14:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,6 +125,44 @@ app.get('/articles/:articleName',function(req, res){
     });
 });
 
+
+function createTemplate(data){
+    var user=data.username;
+var htmlTemplate=`
+    <html>
+    <head>
+        <title>login</title>
+        <meta name="viewpoint" content="with device-width, inital scale=1"/>
+    </head>
+    <body>
+        <div class ="container">
+        <div>
+            <a href ="/">Home</a>
+        </div>
+        <hr>
+        <h3>hi user ${user}</h3>
+        <div>
+        
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+app.get('/holder/:username', function (req, res) {
+      pool.query('SELECT * FROM "user" WHERE username =$1', [req.params.username],function(err,result){
+        if(err)
+            res.status(500).send(err.toString());
+        else
+        if(result.rows.length===0)
+            res.status(404).send('user not found');
+        else{
+            var userData=result.rows[0];
+            res.send(createTemplate(userData));
+        }
+    });
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
